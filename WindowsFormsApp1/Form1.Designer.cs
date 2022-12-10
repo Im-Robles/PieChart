5e7e7a1754c1c5f61eb06fc805fc0f73470956bb
User <User@KAEL>
2022-12-10 23:34:29 +0800

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.yearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.expensesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.analystBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.data = new WindowsFormsApp1.Data();
+            this.dataSet1 = new WindowsFormsApp1.DataSet1();
+            this.pieChart1 = new LiveCharts.WinForms.PieChart();
+            this.btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.analystBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.yearDataGridViewTextBoxColumn,
+            this.expensesDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.analystBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(263, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // yearDataGridViewTextBoxColumn
+            // 
+            this.yearDataGridViewTextBoxColumn.DataPropertyName = "Year";
+            this.yearDataGridViewTextBoxColumn.HeaderText = "Year";
+            this.yearDataGridViewTextBoxColumn.Name = "yearDataGridViewTextBoxColumn";
+            // 
+            // expensesDataGridViewTextBoxColumn
+            // 
+            this.expensesDataGridViewTextBoxColumn.DataPropertyName = "Expenses";
+            this.expensesDataGridViewTextBoxColumn.HeaderText = "Expenses";
+            this.expensesDataGridViewTextBoxColumn.Name = "expensesDataGridViewTextBoxColumn";
+            // 
+            // analystBindingSource
+            // 
+            this.analystBindingSource.DataMember = "Analyst";
+            this.analystBindingSource.DataSource = this.data;
+            // 
+            // data
+            // 
+            this.data.DataSetName = "Data";
+            this.data.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pieChart1
+            // 
+            this.pieChart1.Location = new System.Drawing.Point(33, 23);
+            this.pieChart1.Name = "pieChart1";
+            this.pieChart1.Size = new System.Drawing.Size(265, 237);
+            this.pieChart1.TabIndex = 3;
+            this.pieChart1.Text = "pieChart1";
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(281, 288);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 150);
+            this.btn.TabIndex = 4;
+            this.btn.Text = "Update";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 450);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.pieChart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.analystBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DataSet1 dataSet1;
+        private Data data;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yearDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn expensesDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource analystBindingSource;
+        private LiveCharts.WinForms.PieChart pieChart1;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//using LiveCharts; using LiveCharts.Wpf;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /*Timer timer = new Timer();
+            timer.Interval = (1 * 200); // 1 sec.
+            timer.Tick += new EventHandler(DisplayGraph); //timer1_tick represents the name of Tick Event
+            timer.Start();*/
+        }
+
+        Func<ChartPoint,string> labelPoint =chartpoint=> string.Format("{0} {1:P}", chartpoint.Y, chartpoint.Participation);
+        /*
+        public void DisplayGraph(object sender, EventArgs e)
+        {
+            SeriesCollection series = new SeriesCollection();
+            foreach (var obj in data.Analyst)
+                series.Add(new PieSeries() {
+                    Title=obj.Year.ToString(),
+                    Values=new ChartValues<int> { obj.Expenses },
+                    DataLabels = true,
+                    LabelPoint = labelPoint
+                });
+            pieChart1.Series = series;
+        }*/
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pieChart1.LegendLocation = LegendLocation.Bottom;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            SeriesCollection series = new SeriesCollection();
+            foreach (var obj in data.Analyst)
+                series.Add(new PieSeries() {
+                    Title=obj.Year.ToString(),
+                    Values=new ChartValues<int> { obj.Expenses },
+                    DataLabels = true,
+                    LabelPoint = labelPoint
+                });
+            pieChart1.Series = series;
+        }
+    }
+}
